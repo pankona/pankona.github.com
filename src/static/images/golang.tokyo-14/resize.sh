beedd7b581fa200d6e51bdd4d77cef4b2dfc8ded
pankona <yosuke.akatsuka@gmail.com>
2018-06-16 11:14:45 +0900

    [golangtokyo-14] add resized images

---
+++ src/static/images/golang.tokyo-14/resize.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -e
+
+convert ./01.jpg -resize 15% ./resized_01.jpg
+convert ./02.jpg -resize 15% ./resized_02.jpg
+convert ./03.jpg -resize 15% ./resized_03.jpg
+convert ./04.jpg -resize 15% ./resized_04.jpg
+convert ./05.jpg -resize 15% ./resized_05.jpg
+

40e511047859d90e90a86b3ecc59da53dd008b4d
pankona <yosuke.akatsuka@gmail.com>
2021-03-02 15:17:06 +0900

    add a tool to calculate since last commit

---
+++ tool/sincelastcommit/go.mod
@@ -0,0 +1,5 @@
+module github.com/pankona/pankona.github.com/tool/sincelastcommit
+
+go 1.15
+
+require github.com/go-git/go-git/v5 v5.2.0
---
+++ tool/sincelastcommit/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/go-git/go-git/v5"
+)
+
+func main() {
+	r, err := git.PlainOpen(".")
+	if err != nil {
+		panic(err)
+	}
+	commits, err := r.Log(&git.LogOptions{})
+	if err != nil {
+		panic(err)
+	}
+	commit, err := commits.Next()
+	if err != nil {
+		panic(err)
+	}
+
+	lastCommitDate := commit.Author.When
+	sinceLastCommit := time.Now().Sub(lastCommitDate)
+	if sinceLastCommit.Hours()/24 > 7 {
+		fmt.Printf("最後の更新から %d 日以上経っています。", int(sinceLastCommit.Hours()/24))
+	}
+}

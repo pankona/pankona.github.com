1145f428074d5128858d8e9949440300877fef2b
pankona <yosuke.akatsuka@gmail.com>
2021-03-03 09:31:36 +0900

    fix wording

--- tool/sincelastcommit/main.go
+++ tool/sincelastcommit/main.go
@@ -24,6 +24,6 @@ func main() {
 	lastCommitDate := commit.Author.When
 	sinceLastCommit := time.Now().Sub(lastCommitDate)
 	if sinceLastCommit.Hours()/24 > 7 {
-		fmt.Printf("ブログの最後の更新から %d 日以上経っています。", int(sinceLastCommit.Hours()/24))
+		fmt.Printf("最後のブログ更新から %d 日以上経っています。", int(sinceLastCommit.Hours()/24))
 	}
 }

956c63ae991dbe86c62ba8443aa392f1f180b435
Kenichi Kamiya <kachick1@gmail.com>
2024-02-02 11:21:45 +0900

    Upsert PRs if it has already been created and keeping open state

--- tool/makepr/main.go
+++ tool/makepr/main.go
@@ -18,6 +18,7 @@ func main() {
 	base := flag.String("base", "", "specify base to create pull request")
 	head := flag.String("head", "", "specify head to create pull request")
 	body := flag.String("body", "", "specify pull request body to create pull request")
+	pr_number := flag.Int("pr_number", 0, "specify the pull request number if it has already been created")
 	flag.Parse()
 
 	title := fmt.Sprintf("add a post by %s", *head)
@@ -29,34 +30,57 @@ func main() {
 	tc := oauth2.NewClient(ctx, ts)
 	client := github.NewClient(tc)
 
-	pr, resp, err := client.PullRequests.Create(context.Background(), "pankona", "pankona.github.com",
-		&github.NewPullRequest{
-			Title: &title,
-			Head:  head,
-			Base:  base,
-			Body:  body,
-		})
-	var e *github.ErrorResponse
-	if errors.As(err, &e) {
-		for _, v := range e.Errors {
-			if strings.Contains(v.Message, "A pull request already exists") {
-				log.Printf("pull request already exists. do nothing")
-				return
+	if *pr_number == 0 {
+		pr, resp, err := client.PullRequests.Create(context.Background(), "pankona", "pankona.github.com",
+			&github.NewPullRequest{
+				Title: &title,
+				Head:  head,
+				Base:  base,
+				Body:  body,
+			})
+		var e *github.ErrorResponse
+		if errors.As(err, &e) {
+			for _, v := range e.Errors {
+				if strings.Contains(v.Message, "A pull request already exists") {
+					log.Printf("pull request already exists. do nothing")
+					return
+				}
 			}
+		} else if err != nil {
+			panic(err)
 		}
-	} else if err != nil {
-		panic(err)
-	}
 
-	defer resp.Body.Close()
+		defer resp.Body.Close()
 
-	if resp.StatusCode != 201 {
-		buf, err := ioutil.ReadAll(resp.Body)
+		if resp.StatusCode != 201 {
+			buf, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			panic(fmt.Sprintf("failed to create pull request. status code: %d, body: %s", resp.StatusCode, buf))
+		}
+
+		log.Printf("pull request created: %s", *pr.IssueURL)
+	} else {
+		pr, resp, err := client.PullRequests.Edit(context.Background(), "pankona", "pankona.github.com", *pr_number,
+			&github.PullRequest{
+				Title: &title,
+				Body:  body,
+			})
 		if err != nil {
 			panic(err)
 		}
-		panic(fmt.Sprintf("failed to create pull request. status code: %d, body: %s", resp.StatusCode, buf))
-	}
 
-	log.Printf("pull request created: %s", *pr.IssueURL)
+		defer resp.Body.Close()
+
+		if resp.StatusCode != 201 {
+			buf, err := ioutil.ReadAll(resp.Body)
+			if err != nil {
+				panic(err)
+			}
+			panic(fmt.Sprintf("failed to update pull request. status code: %d, body: %s", resp.StatusCode, buf))
+		}
+
+		log.Printf("pull request updated: %s", *pr.IssueURL)
+	}
 }

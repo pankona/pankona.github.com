40797e3f2c2e4c166944395c16fee4988b1397dc
Kenichi Kamiya <kachick1@gmail.com>
2024-04-04 17:50:42 +0900

    Prefer stable pkgs for bash and basics in Nix
    Import https://github.com/kachick/anylang-template/pull/80

--- flake.nix
+++ flake.nix
@@ -1,14 +1,16 @@
 {
   inputs = {
     # How to update the revision: `nix flake update --commit-lock-file`
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.11";
+    edge-nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, edge-nixpkgs, flake-utils }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        edge-pkgs = edge-nixpkgs.legacyPackages.${system};
       in
       {
         devShells.default = with pkgs;
@@ -17,19 +19,20 @@
               # https://github.com/NixOS/nix/issues/730#issuecomment-162323824
               bashInteractive
 
-              hugo
-              go_1_22
               gnumake
               coreutils
               peco
-              typos
-              dprint
               yamlfmt
+              typos
               imagemagick
               actionlint
               nil
               nixpkgs-fmt
               vim
+
+              edge-pkgs.hugo
+              edge-pkgs.go_1_22
+              edge-pkgs.dprint
             ];
           };
       });
